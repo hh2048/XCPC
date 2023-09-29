d5b271bf3dce08f4041e92d1fa4d368098d952fa
hh2048 <140591203+hh2048@users.noreply.github.com>
2023-09-29 19:18:15 +0800

    2023.09.29 Update

---
+++ 常用在线模板汇总/01 - 在线头文件（简洁版）.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifndef ONLINE_JUDGE
+    #include <bits/wida.h>
+#endif
+
+
+signed main() {
+    int Task = 1;
+    for (cin >> Task; Task; Task--) {
+        
+    }
+}
+
+int __OI_INIT__ = []() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    cout.tie(0);
+    cout << fixed << setprecision(12);
+    return 0;
+}();
