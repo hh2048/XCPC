2d829d76a850c70931c631dc3edcefa4bca6b5e6
WIDA <140591203+hh2048@users.noreply.github.com>
2024-02-12 14:14:46 +0800

    2024.02.12 Update 更新一个函数

--- 03 - jiangly模板收集/01 - 杂类/02 - 常用库函数重载.cpp
+++ 03 - jiangly模板收集/01 - 杂类/02 - 常用库函数重载.cpp
@@ -1,3 +1,6 @@
+using i64 = long long;
+using i128 = __int128;
+
 i64 ceilDiv(i64 n, i64 m) {
     if (n >= 0) {
         return (n + m - 1) / m;
@@ -19,4 +22,8 @@ void chmax(T &a, T b) {
     if (a < b) {
         a = b;
     }
+}
+
+i128 gcd(i128 a, i128 b) {
+    return b ? gcd(b, a % b) : a;
 }
